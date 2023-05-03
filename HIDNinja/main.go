f2225833d616feec5ad9971fa0103106c05b759c
Marc <marc.helbling@gmail.com>
2023-05-03 10:59:33 +0300

    refine code and split in more functions

--- HIDNinja/main.go
+++ HIDNinja/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// Defining the read and write buffer size for the upgrader
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -20,34 +21,53 @@ func executePayload(payloadString string) bool {
 	return true
 }
 
-func main() {
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../PayloadInterface/index.html")
+}
 
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade the connection to a websocket
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Client Connected")
+
+	reader(conn)
+
+}
+
+//reader function that listens for incoming payloads
+func reader(conn *websocket.Conn) {
+	// keep listening for incoming payloads
+	for {
+		//read incoming payload
+		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
-			log.Fatal(err)
+			return
 		}
 
-		for {
+		//print received message to console
+		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
 
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
+		if err = conn.WriteMessage(msgType, msg); err != nil {
+			return
 		}
-	})
+	}
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../PayloadInterface/index.html")
-	})
+func setupRoutes() {
+	http.HandleFunc("/echo", wsEndpoint)
+	http.HandleFunc("/", homePage)
+}
 
+func main() {
+	setupRoutes()
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
