44bf8af450bbe51fe4dfc5f9ae7a5a7dd716c4e8
mh37 <78908148+mh37@users.noreply.github.com>
2023-05-12 16:17:42 +0300

    setup script fixes

--- HIDNinja/setup.sh
+++ HIDNinja/setup.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# Echo commands to stdout.
+set -x
+
+# Exit on first error.
+set -e
+
+# Treat undefined environment variables as errors.
+set -u
+
+
 #enable deice tree overlay and add dtoverlay=dwc2 to /boot/config.txt
-echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+if ! grep 'dtoverlay=dwc2' /boot/config; then
+  echo "dtoverlay=dwc2" >> /boot/config.txt
+fi
 # Add dwc2 and libcomposite to /etc/modules
-echo "dwc2" | sudo tee -a /etc/modules
-echo "libcomposite" | sudo tee -a /etc/modules
+if ! grep dwc2 /etc/modules; then
+  echo "dwc2" >> /etc/modules
+fi
 
 ENABLE_RPI_HID_PATH=/opt/enable-rpi-hid
 ENABLE_RPI_HID_DIR=$(dirname $ENABLE_RPI_HID_PATH)
@@ -13,8 +26,12 @@ mkdir -p "$ENABLE_RPI_HID_DIR"
 cp enableHIDmode "$ENABLE_RPI_HID_PATH"
 chmod +x "$ENABLE_RPI_HID_PATH"
 
-cp usb-gadget.service usr/lib/systemd/system/usb-gadget.service
+sed -e "s@/usr/bin/hidninja@${ENABLE_RPI_HID_PATH}@g" \
+  usb-gadget.service > /lib/systemd/system/usb-gadget.service
 
 # start service and enable run on boot
 systemctl daemon-reload
 systemctl enable usb-gadget.service
+
+# reboot system
+reboot
