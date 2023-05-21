99d308ff22d8f4f58641bfa531e414cbad8b4173
mh37 <78908148+mh37@users.noreply.github.com>
2023-05-21 18:11:56 +0300

    split websocket into own file

--- HIDNinja/main.go
+++ HIDNinja/main.go
@@ -6,8 +6,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/gorilla/websocket"
 )
 
 var scanCodes = map[string]byte{
@@ -191,12 +189,6 @@ var scanCodes = map[string]byte{
 	"MEDIA_CALC":         0xfb,
 }
 
-// Defining the read and write buffer size for the upgrader
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
 // Translates char/key into HID compatible code
 func translationLayer(s string) byte {
 	if val, ok := scanCodes[s]; ok {
@@ -239,56 +231,6 @@ func executePayload(payloadString string) bool {
 	}
 
 	return true
-}
-
-// Serve the index.html for the payload web interface
-func homePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "../PayloadInterface/index.html")
-}
-
-// WebSocket endpoint
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-	// upgrade the connection to a websocket
-	conn, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Client Connected")
-
-	reader(conn)
-}
-
-// reader function that listens for incoming payloads
-func reader(conn *websocket.Conn) {
-	// keep listening for incoming payloads
-	for {
-		//read incoming payload
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			return
-		}
-
-		//print received message to console
-		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-		if err = conn.WriteMessage(msgType, msg); err != nil {
-			return
-		}
-
-		//execute payload
-		executePayload(string(msg))
-	}
-}
-
-// Defines the routes, such as the WebSocket Endpoint and Homepage
-func setupRoutes() {
-	http.HandleFunc("/echo", wsEndpoint)
-	http.HandleFunc("/", homePage)
 }
 
 func main() {
---
+++ HIDNinja/websocket.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// Defining the read and write buffer size for the upgrader
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Serve the index.html for the payload web interface
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../PayloadInterface/index.html")
+}
+
+// WebSocket endpoint
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade the connection to a websocket
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Client Connected")
+
+	reader(conn)
+}
+
+// reader function that listens for incoming payloads
+func reader(conn *websocket.Conn) {
+	// keep listening for incoming payloads
+	for {
+		//read incoming payload
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		//print received message to console
+		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+		if err = conn.WriteMessage(msgType, msg); err != nil {
+			return
+		}
+
+		//execute payload
+		executePayload(string(msg))
+	}
+}
+
+// Defines the routes, such as the WebSocket Endpoint and Homepage
+func setupRoutes() {
+	http.HandleFunc("/echo", wsEndpoint)
+	http.HandleFunc("/", homePage)
+}
